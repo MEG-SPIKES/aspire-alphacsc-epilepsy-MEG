4cc6554a8671246dc43ef24fd5131fdbc9d3820e
vagechirkov <vagechirkov@gmail.com>
2021-11-30 00:10:27 +0100

    plot supplement with all stcs

--- analysis/utils/plot_paper_images.py
+++ analysis/utils/plot_paper_images.py
@@ -1,3 +1,5 @@
+import matplotlib.pylab as plt
+import mne
 import numpy as np
 import pandas as pd
 from megspikes.casemanager.casemanager import CaseManager
@@ -18,3 +20,40 @@ def fig4_one_row(n: int, case: CaseManager, fig4_table: pd.DataFrame, subj: int,
     fig4_table.loc[n, 'distance_resection'] = dist_to_resection
     n += 1
     return fig4_table, n
+
+
+def stc_surfaces(subjects_dir, stc_resection, stc_manual=None, stc_peak=None, stc_slope=None):
+    # pipeline roc
+    fig, ax = plt.subplots(nrows=4, ncols=4*2, figsize=(25, 15), dpi=300)
+    surfer_kwargs = dict(hemi='split',  surface='inflated',  # 'white'
+                         views=['lat'], spacing='ico4',
+                         time_viewer=False, #  backend='mayavi',
+                         subjects_dir=subjects_dir, colormap='Greens', colorbar=False,
+                         background='w', foreground='k',  # , size=(500, 2000),
+                         smoothing_steps='nearest', alpha=1, verbose='error',
+                         add_data_kwargs={"fmin": 0, "fmid": 0.9, "fmax": 1})
+    col = -1
+    cmaps = ['Blues', 'Reds', 'Reds', 'Reds']
+    titles = ['Resection', 'Manual', 'Automated (peak)', 'Automated (slope)']
+    stcs = [stc_resection, stc_manual, stc_peak, stc_slope]
+    for stc, cmap, title in zip(stcs, cmaps, titles):
+        for hemi in ['rh', 'lh']:
+            col += 1
+            for row, view in enumerate(['lat', 'med', 'fro', 'par']):
+                ax[row, col].axis('off')
+                ax[row, col].get_xaxis().set_visible(False)
+                ax[row, col].get_yaxis().set_visible(False)
+                if not isinstance(stc, mne.SourceEstimate):
+                    continue
+                surfer_kwargs['hemi'] = hemi
+                surfer_kwargs['views'] = view
+                surfer_kwargs['colormap'] = cmap
+                brain = stc.plot(**surfer_kwargs)
+                im = brain.screenshot()
+                brain.close()
+                ax[row, col].imshow(im)
+                if view == 'lat':
+                    ax[row, col].set_title(f"{title} {hemi}")
+    plt.tight_layout()
+    return fig
+
